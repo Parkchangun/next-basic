ef847117e438daea4d9620a10053b993c1c688ca
parkchangeon <qkrcksrjs@gmail.com>
2022-05-05 17:05:58 +0900

    hide API_KEY

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { SEO } from 'src/components'
 
-const API_KEY = '10923b261ba94d897ac6b81148314a3f'
-
 const Home: NextPage = () => {
   // (async () => {
   //   const response = await (await fetch(`fdfdfdfadf`)).json()
@@ -14,28 +12,44 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     ;(async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json()
+      const { results } = await (await fetch(`/api/movies`)).json()
 
       setMovies(results)
     })()
   }, [])
 
   return (
-    <>
+    <div className='container'>
       <SEO title='Home' />
-      <h1>Hello</h1>
-      {/* <Image src='/vercel.svg' alt='vercel' layout='fill' /> */}
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie: any) => (
-        <div key={movie.id}>
+        <div className='movie' key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
-    </>
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
   )
 }
 
